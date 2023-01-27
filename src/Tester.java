35b823e3142b5ba69b99dcdd54b8f10f172a514d
Eduard <eduard.prog22@gmail.com>
2023-01-27 15:01:54 +0100

    First try

---
+++ src/Tester.java
@@ -0,0 +1,13 @@
+public class Tester {
+    public Tester(){
+    }
+    public static void main(String[] args){
+        String string = "tup tuup tuuup tuuuup";
+        
+        System.out.println(string.replaceAll("u{2,3}", "x"));
+
+
+
+
+    }
+}
